86a9dbcf3d5a2efc2061e8efe6dc78e2a8829783
henryf12345687 <hjforst@students.unwsp.edu>
2025-10-17 16:15:18 +0000

    Week 7 Programs

--- program_1.py
+++ program_1.py
@@ -1,4 +1,30 @@
 # Program #1: Rainfall
+#Henry Forst
+#10/16/25
 # Design a program that lets the user enter the total rainfall for each of 12 months into a list.
 # The program should calculate and display the total rainfall for the year, 
 # the average monthly rainfall, # and the months with the highest and lowest amounts.
+months = 12
+def main():
+    month_names = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    rainfall = [0] * months
+    print("Enter the amount of rainfall (inches) in each month ")
+    for index in range(len(rainfall)):
+        rainfall[index] = float(input(f'{month_names[index]}:'))
+    #Display enrty
+    total = sum(rainfall)
+    #print total
+    print('The total rainfall for the year was:',total , 'inches')
+    average = total / months
+    #Print avg
+    print(f'The average rainfall per month was: {round(average,): .2f}  inches')
+    #Print least amt of rain
+    minRain = min(rainfall)
+    minMonth = month_names[rainfall.index(minRain)]
+    print(f'The least amount inches of rain in a month was: {minRain} inches in {minMonth}')
+    #print most amt of rain
+    maxRain = max(rainfall)
+    maxMonth = month_names[rainfall.index(maxRain)]
+    print(f'The least amount inches of rain in a month was: {maxRain} inches in {maxMonth}')
+if __name__ == '__main__':
+    main()
--- program_4.py
+++ program_4.py
@@ -1,10 +1,28 @@
 # Program #4: Coordinates
+#Henry Forst
+#10/16/25
 # Write a distance function that will take two 3-dimensional coordinates (as input) 
 # and will return (as output) the distance between those points in space.  
 # The 3-dimensional coordinates must be stored as tuples.
+import math
+def distance(coord1, coord2):
+    x1, y1, z1 = coord1
+    x2, y2, z2 = coord2 
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+def main():
+    try:
+        coord1 = eval((input("Enter the first coordinate (x1, y1, z1): ")))
+        coord2 = eval((input("Enter the second coordinate (x2, y2, z2): ")))
+        if len(coord1) != 3 or len(coord2) != 3:
+            print("Each tuple needs to have three variables")
+            return
+        print (f'The distance between the points is:{distance(coord1, coord2): .2f} spaces')
+    except Exception as er:
+        print(f"Error: {er}")
 
 # Now write a mainline that has the user enter the two tuples.  
 # The mainline calls the distance function and stores the distance in a variable.  The mainline then displays the distance.  
 # Also include exception handling to deal with faulty input.
 # The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
 #    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+main()
